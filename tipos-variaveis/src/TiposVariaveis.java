b45bfe6fd8926b9372da2827b6d7a31ab8dcb826
Fega <fernando_gouveia@live.com>
2023-03-06 21:32:14 -0300

    Conhencendo tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;  // fazendo um casting
+        System.out.println(numeroCurto2);
+        
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.println(numero);
+        // declarando que uma variável não vai ser mudada nunca = Constante.
+        final double VALOR_DE_PI = 3.14;
     }
 }
